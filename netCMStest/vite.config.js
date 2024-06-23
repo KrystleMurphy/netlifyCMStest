b2c22d0cd5a9a69f62e0d65ea03b6105279dc254
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-06-23 17:28:22 +0100

    changing vite config file

--- netCMStest/vite.config.js
+++ netCMStest/vite.config.js
@@ -1,22 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-// vite.config.js
-
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-export default {
-  plugins: [
-    reactRefresh()
-  ],
   build: {
     rollupOptions: {
       external: ['react-router-dom']
     }
   }
-}
+});
